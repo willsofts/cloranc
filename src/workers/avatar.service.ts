50c512f8664d5d07149fb11f241967234bae1316
tassan_oro <32536248+tassun@users.noreply.github.com>
2025-06-28 20:37:02 +0700

    Update avatar.service.ts

--- src/workers/avatar.service.ts
+++ src/workers/avatar.service.ts
@@ -8,27 +8,38 @@ const AvatarService : ServiceSchema = {
         image(ctx: any) {
             this.logger.debug("ctx",ctx);
             let userid = ctx.params.userid;
+            let photoimage = ctx.params.photoimage;
             if(!userid || userid.trim().length==0) userid = "anonymous";
             let path = "assets/metronic/media/users";
             let directory = "./public/"+path;
             let avatar = "";
             try {
-                const files = fs.readdirSync(directory);
                 let found = false;
-                for(let file of files) {
-                    if(file.indexOf(userid)>=0) {
+                if(photoimage && photoimage.trim().length>0) {
+                    let imgpath = "img/avatar";
+                    let imgfile = "./public/"+imgpath+"/"+photoimage;
+                    if (fs.existsSync(imgfile)) {
                         found = true;
-                        avatar = "/"+path+"/"+file;
+                        avatar = "/"+imgpath+"/"+photoimage;
                     }
                 }
                 if(!found) {
-                    if(files && files.length > 1) {
-                        const hash = crypto.createHash('sha256').update(userid).digest();
-                        const hint = hash.readUInt32BE(0);
-                        let index = hint % files.length;
-                        avatar = "/"+path+"/"+files[index];
-                    } else {
-                        avatar = "/"+path+"/"+files[0];
+                    const files = fs.readdirSync(directory);
+                    for(let file of files) {
+                        if(file.indexOf(userid)>=0) {
+                            found = true;
+                            avatar = "/"+path+"/"+file;
+                        }
+                    }
+                    if(!found) {
+                        if(files && files.length > 1) {
+                            const hash = crypto.createHash('sha256').update(userid).digest();
+                            const hint = hash.readUInt32BE(0);
+                            let index = hint % files.length;
+                            avatar = "/"+path+"/"+files[index];
+                        } else {
+                            avatar = "/"+path+"/"+files[0];
+                        }
                     }
                 }
             } catch(ex) { this.logger.error(ex); }
