f84cac1ab6b8a35703dff6af064bd88639823571
tassan_oro <32536248+tassun@users.noreply.github.com>
2025-07-01 13:29:44 +0700

    change config folder

--- public/default.js
+++
@@ -1,7 +0,0 @@
-var appVariables = {
-    ALLOW_AUTHEN_SAML: "true",
-    MULTI_LANGUAGES: ["EN","TH"],
-    BASE_CSS: "",
-    DEFAULT_LANGUAGE: "EN",
-	META_INFO: { chat: true, notify: true, language: true, LOAD_LABEL: true, LOAD_MESSAGE_CODE: true }
-};
