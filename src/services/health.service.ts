30684bab21315c22d2d37fa37508d30378f1b5be
tassan_oro <32536248+tassun@users.noreply.github.com>
2025-11-25 11:07:12 +0700

    Update health.service.ts

--- src/services/health.service.ts
+++ src/services/health.service.ts
@@ -1,14 +1,40 @@
 import { ServiceSchema } from "moleculer";
 const os = require("os");
+const dns = require("dns");
+
+function getLocalIP() {
+  const nets = os.networkInterfaces();
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+      if (net.family === "IPv4" && !net.internal) {
+        return net.address;
+      }
+    }
+  }
+}
+
+function getLocalAddress() {
+    return new Promise((resolve, reject) => {
+        dns.lookup(os.hostname(), { family: 4 }, (err:any, address:string) => {
+            if (err) reject(err);
+            resolve(address);
+        });
+    });
+}
 
 const HealthCheckService : ServiceSchema = {
     name: "health",
     actions: {
-        check(ctx: any) {
+        async check(ctx: any) {
             ctx.meta.$responseRaw = true; 
-            ctx.meta.$responseType = "application/json";    
-            return {status: "OK", hostname: os.hostname(), pid: process.pid};
+            ctx.meta.$responseType = "application/json";
+            let addr = await getLocalAddress();
+            let ip = getLocalIP();
+            let info = {hostname: os.hostname(), ip: [addr,ip], pid: process.pid};
+            ctx.service?.logger?.info("health/check:",info);
+            return {status: "OK", ...info};
         },
     },
 };
+
 export = HealthCheckService;
